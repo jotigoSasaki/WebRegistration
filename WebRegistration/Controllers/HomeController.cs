a98d8145bd5cb96974c0e79068ae44ad86cf9222
ashot <ashot@DESKTOP-C5O4368>
2021-08-25 20:01:40 +0400

    Add project files.

---
+++ WebRegistration/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public ViewResult SignIn() => View();
+
+        public ViewResult SignUp() => View();
+    }
+}
---
+++ WebRegistration/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRegistration.models
+{
+    public class User
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+
+    }
+}
